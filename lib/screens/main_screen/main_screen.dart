0f8048c2b9711de4ffa85d33b6d1e655f216d2bc
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-20 15:56:40 +0500

    carousel slider is done

---
+++ lib/screens/main_screen/main_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'subdir/widgets.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          CustomCarousel(),
+        ],
+      ),
+    );
+  }
+}
